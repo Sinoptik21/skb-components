7c9cb9b5da983422d9745d234055043d04c40795
Sinoptik21 <anton@bokus.ru>
2017-05-10 13:39:28 +0300

    Updated Storybox and lsk-build; deleted Avatar.css

---
+++ .storybook/params.js
@@ -0,0 +1,10 @@
+export default {
+  modules: require('glob!./glob.txt'),
+  options: {
+    name: 'Project0',
+    url: 'https://github.com/Sinoptik21',
+  },
+  info: false,
+  knob: true,
+  utils: {},
+};
